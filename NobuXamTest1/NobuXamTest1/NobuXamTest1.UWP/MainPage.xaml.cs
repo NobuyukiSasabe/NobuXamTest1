03a55a6fa6b54d4a006ecb650842ac12aea5b1b8
NobuyukiSasabe <sasabe@goodf.co.jp>
2017-06-25 17:08:04 +0900

    コミット

---
+++ NobuXamTest1/NobuXamTest1/NobuXamTest1.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NobuXamTest1.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new NobuXamTest1.App());
+        }
+    }
+}
